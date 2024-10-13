56f329bdefcb0ee93746ee482899eaeffca6ee1c
tejesavi <125518577+tejesavi@users.noreply.github.com>
2024-10-13 14:51:32 -0400

    Update Program.cs

--- FileUploaderApp/Program.cs
+++ FileUploaderApp/Program.cs
@@ -1,17 +1,23 @@
 using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// Add Azure Blob Storage services with dependency injection.
+// Add Azure Blob Storage services
 builder.Services.AddSingleton(x =>
 {
-    // Get the connection string from environment variables
-    string connectionString = builder.Configuration.GetConnectionString("AzureBlobStorage") ?? throw new InvalidOperationException("Connection string not found.");
+    // Retrieve the connection string from environment variables
+    string connectionString = builder.Configuration.GetConnectionString("AzureBlobStorage");
+    
+    // If the connection string is not found in appsettings.json, retrieve it from environment variables
+    if (string.IsNullOrEmpty(connectionString))
+    {
+        connectionString = Environment.GetEnvironmentVariable("AzureBlobStorage");
+    }
+    
     return new BlobServiceClient(connectionString);
 });
 
